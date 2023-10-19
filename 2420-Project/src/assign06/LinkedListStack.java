efe01686d9fa54aed9707869d65077791d6492e9
ocatten <123414884+ocatten@users.noreply.github.com>
2023-10-19 13:59:31 -0700

    Finished file outline

--- 2420-Project/src/assign06/LinkedListStack.java
+++ 2420-Project/src/assign06/LinkedListStack.java
@@ -2,24 +2,51 @@ package assign06;
 
 import java.util.NoSuchElementException;
 
-public class LinkedListStack {
+
+/**
+ * This is a stack data structure with a SinglyLinkedList object used as the backing structure.
+ * This structure is a First-In, Last-Out list of elements that can push, peek, or pop the top
+ * of the stack. Used in The WebBrowser application.
+ * 
+ * @author Parker Catten & Everett Oglesby
+ * @version 10:19:23
+ * 
+ * @param <E> - the type of elements contained in the stack
+ */
+public class LinkedListStack<E> {
+	
+	
+	// Fields
+	SinglyLinkedList<E> backingList;
+	
+	
+	/**
+	 * @Constructor that sets up the backing SinglyLinkedList member variable for the stack.
+	 */
+	public LinkedListStack () {
+		backingList = new SinglyLinkedList<E>();
+	}
 
 	
 	
 	/**
-	 * Removes all of the elements from the stack.
+	 * Removes all of the elements from the stack by clearing the backingList
 	 */
 	public void clear() {
-		
+		backingList.clear();
 	}
-
+	
+	
+	
 	/**
-	 * @return true if the stack contains no elements; false, otherwise.
+	 * @return true if the stack is empty, false otherwise. Uses backingList helper method to check.
 	 */
 	public boolean isEmpty() {
-		return false;
+		return backingList.isEmpty();
 	}
 
+	
+	
 	/**
 	 * Returns, but does not remove, the element at the top of the stack.
 	 * 
@@ -27,9 +54,11 @@ public class LinkedListStack {
 	 * @throws NoSuchElementException if the stack is empty
 	 */
 	public E peek() throws NoSuchElementException{
-		return null;
+		return backingList.getFirst(); // Uses the head of the list as top of stack
 	}
-
+	
+	
+	
 	/**
 	 * Returns and removes the item at the top of the stack.
 	 * 
@@ -37,22 +66,25 @@ public class LinkedListStack {
 	 * @throws NoSuchElementException if the stack is empty
 	 */
 	public E pop() throws NoSuchElementException{
-		return null;
+		return backingList.deleteFirst(); // Uses the head of the list as top of stack
 	}
-
+	
+	
+	
 	/**
 	 * Adds a given element to the stack, putting it at the top of the stack.
 	 * 
 	 * @param element - the element to be added
 	 */
 	public void push(E element) {
-
+		
+		backingList.insertFirst(element); // Uses the head of the list as top of stack
 	}
 	
 	/**
 	 * @return the number of elements in the stack
 	 */
 	public int size() {
-		return 0;
+		return backingList.size(); // Use the backing list's internal method.
 	}
 }
