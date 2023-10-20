a1029efe4c9288dc6cf847669477a4881eb1e981
ocatten <123414884+ocatten@users.noreply.github.com>
2023-10-19 19:38:07 -0700

    testers

--- 2420-Project/src/assign06/LinkedListStackTester.java
+++ 2420-Project/src/assign06/LinkedListStackTester.java
@@ -1,5 +1,345 @@
 package assign06;
 
-public class LinkedListStackTester {
+import static org.junit.Assert.assertEquals; 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import org.junit.Test;
+
+
+/**
+ * Tester class for the LinkedListStack
+ * 
+ * @author Parker Catten & Everett Oglesby
+ * @version 10:19:23
+ */
+public class LinkedListStackTester {
+	
+//============================================================= SET_UP ===============================================================================
+	
+	private static LinkedListStack<String> emptyStack;
+	private static LinkedListStack<String> smallStack;
+	private static LinkedListStack<Integer> largeStack;
+	
+	public static void setup () {
+		
+		emptyStack = new LinkedListStack<String>();
+		
+		smallStack = new LinkedListStack<String>();
+		
+		smallStack.push("z");
+		smallStack.push("e");
+		smallStack.push("b");
+		smallStack.push("x");
+		smallStack.push("u");
+		smallStack.push("s");
+		smallStack.push("t");
+		smallStack.push("i");
+		smallStack.push("m");
+		
+		
+		largeStack = new LinkedListStack<Integer>();
+		
+		for(int i = 0; i < 10000; i++) {
+			
+			largeStack.push(i);
+		}
+	}
+	
+	
+//=========================================================== CLEAR TESTS =============================================================================
+	
+	
+	@Test
+	public static void clearTestOnEmpty () {
+		setup();
+		emptyStack.clear();
+	}
+	
+	
+	
+	@Test
+	public static void clearTestOnSmall () {
+		setup();
+		
+		assertEquals(smallStack.size(), 9);
+		assertFalse(smallStack.isEmpty());
+		
+		smallStack.clear();
+		assertTrue(smallStack.isEmpty());
+	}
+	
+	
+	
+	@Test
+	public static void clearTestOnLarge () {
+		setup();
+		
+		assertEquals(largeStack.size(), 10000);
+		assertFalse(largeStack.isEmpty());
+		
+		largeStack.clear();
+		assertTrue(largeStack.isEmpty());
+	}
+	
+	
+	
+//========================================================== IS_EMPTY TESTS ============================================================================
+
+
+	@Test
+	public static void isEmptyTestOnEmpty () {
+		setup();
+		
+		assertTrue(emptyStack.isEmpty());
+		emptyStack.push("a");
+		assertFalse(emptyStack.isEmpty());
+	}
+	
+	
+	
+	@Test
+	public static void isEmptyTestOnSmall () {
+		setup();
+		
+		assertFalse(smallStack.isEmpty());
+		
+		smallStack.pop();
+		smallStack.pop();
+		smallStack.pop();
+		smallStack.pop();
+		smallStack.pop();
+		smallStack.pop();
+		smallStack.pop();
+		smallStack.pop();
+		smallStack.pop();
+		
+		assertTrue(smallStack.isEmpty());
+	}
+	
+	
+	
+	@Test
+	public static void isEmptyTestOnLarge () {
+		setup();
+		
+		assertFalse(largeStack.isEmpty());
+		
+		for(int i = 0; i < 10000; i++) {
+			
+			largeStack.pop();
+		}
+		
+		assertTrue(largeStack.isEmpty());
+	}
+	
+	
+	
+//=========================================================== PEEK TESTS =============================================================================
+	
+	
+	@Test
+	public static void peekTestOnEmpty () {
+		setup();
+		
+		try {
+			emptyStack.peek();
+		} catch (Exception e) {
+			
+			assertTrue(true);
+		}
+		
+	}
+	
+	
+	
+	@Test
+	public static void peekTestOnSmall () {
+		setup();
+		
+		assertEquals("m", smallStack.peek());
+		assertEquals("m", smallStack.peek());
+		
+		smallStack.pop();
+		
+		assertEquals("i", smallStack.peek());
+		assertEquals("i", smallStack.peek());
+	}
+	
+	
+	
+	@Test
+	@SuppressWarnings("removal")
+	public static void peekTestOnLarge () {
+		setup();
+		
+		assertEquals(new Integer(9999), largeStack.peek());
+		assertEquals(new Integer(9999), largeStack.peek());
+		
+		largeStack.pop();
+		
+		assertEquals(new Integer(9998), largeStack.peek());
+		assertEquals(new Integer(9998), largeStack.peek());
+	}
+	
+	
+	
+//=========================================================== POP TESTS ==============================================================================
+	
+	
+	@Test
+	public static void popTestOnEmpty () {
+		setup();
+		
+		try {
+			emptyStack.pop();
+		} catch (Exception e) {
+			
+			assertTrue(true);
+		}
+		
+		
+	}
+	
+	
+	
+	@Test
+	public static void popTestOnSmall () {
+		setup();
+		
+		assertEquals("m", smallStack.pop());
+		assertEquals("i", smallStack.pop());
+		assertEquals("u", smallStack.pop());
+		assertEquals("s", smallStack.pop());
+		assertEquals("t", smallStack.pop());
+		assertEquals("x", smallStack.pop());
+		assertEquals("b", smallStack.pop());
+		assertEquals("e", smallStack.pop());
+		assertEquals("z", smallStack.pop());
+		
+		try {
+			smallStack.pop();
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+		
+		assertTrue(smallStack.isEmpty());
+	}
+	
+	
+	
+	@Test
+	public static void popTestOnLarge () {
+		setup();
+		
+		for(Integer i = 9999; i >= 0; i--) {
+			assertEquals(i, largeStack.pop());
+		}
+		
+		try {
+			largeStack.pop();
+		} catch (Exception e) {
+			assertTrue(true);
+		}
+		
+		assertTrue(largeStack.isEmpty());
+	}
+	
+	
+	
+//=========================================================== PUSH TESTS =============================================================================
+	
+	
+	@Test
+	public static void pushTestOnEmpty () {
+		setup();
+		
+		assertTrue(emptyStack.isEmpty());
+		assertEquals(0, emptyStack.size());
+		
+		emptyStack.push("a");
+		
+		assertEquals("a", emptyStack.peek());
+		assertFalse(emptyStack.isEmpty());
+		assertEquals(1, emptyStack.size());
+	}
+	
+	
+	
+	@Test
+	public static void pushTestOnSmall () {
+		setup();
+		
+		assertEquals("m", smallStack.peek());
+		assertEquals(9, smallStack.size());
+		
+		smallStack.push("y");
+		
+		assertEquals("y", emptyStack.peek());
+		assertEquals(10, emptyStack.size());
+	}
+	
+	
+	
+	@Test
+	@SuppressWarnings("removal")
+	public static void pushTestOnLarge () {
+		setup();
+		
+		assertEquals(new Integer(9999), largeStack.peek());
+		assertEquals(10000, largeStack.size());
+		
+		largeStack.push(11111);
+		assertEquals(new Integer(11111), largeStack.peek());
+		assertEquals(10001, largeStack.size());
+	}
+	
+	
+	
+//=========================================================== SIZE TESTS =============================================================================
+	
+	
+	@Test
+	public static void sizeTestOnEmpty () {
+		setup();
+		
+		assertEquals(0, emptyStack.size());
+		emptyStack.push("a");
+		assertEquals(1, emptyStack.size());
+		emptyStack.pop();
+		assertEquals(0, emptyStack.size());
+	}
+	
+	
+	
+	@Test
+	public static void sizeTestOnSmall () {
+		setup();
+		
+		assertEquals(9, smallStack.size());
+		smallStack.push("y");
+		assertEquals(10, smallStack.size());
+		smallStack.pop();
+		assertEquals(9, smallStack.size());
+		
+		smallStack.clear();
+		assertEquals(0, smallStack.size());
+	}
+	
+	
+	
+	@Test
+	public static void sizeTestOnLarge () {
+		setup();
+		
+		assertEquals(100000, largeStack.size());
+		largeStack.push(11);
+		assertEquals(100001, largeStack.size());
+		smallStack.pop();
+		assertEquals(100000, largeStack.size());
+		
+		largeStack.clear();
+		assertEquals(0, largeStack.size());
+	}
+	
 }
--- 2420-Project/src/assign06/WebBrowserTester.java
+++ 2420-Project/src/assign06/WebBrowserTester.java
@@ -1,5 +1,204 @@
 package assign06;
 
-public class WebBrowserTester {
+import static org.junit.Assert.assertEquals; 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Tester class for the WebBrowser
+ * 
+ * @author Parker Catten & Everett Oglesby
+ * @version 10:19:23
+ */
+public class WebBrowserTester {
+	
+//============================================================= SET_UP ===============================================================================
+	
+	// Fields
+	private static WebBrowser emptyBrowser;
+	private static WebBrowser noHistory;
+	private static WebBrowser noForward;
+	private static WebBrowser fullBrowser;
+	
+	
+	public static void setup () throws MalformedURLException {
+		
+		emptyBrowser = new WebBrowser();
+		
+		
+		noHistory = new WebBrowser();
+		
+		noHistory.visit(new URL("http://www.example.com/docs/resource1.html"));
+		noHistory.visit(new URL("https://www.google.com/"));
+		noHistory.visit(new URL("https://www.youtube.com/"));
+		
+		noHistory.back();
+		noHistory.back();
+		noHistory.back();
+		
+		
+		noForward = new WebBrowser();
+		
+		noForward.visit(new URL("http://www.example.com/docs/resource1.html"));
+		noForward.visit(new URL("https://www.google.com/"));
+		noForward.visit(new URL("https://www.youtube.com/"));
+		
+		
+		fullBrowser = new WebBrowser();
+		
+		fullBrowser.visit(new URL("http://www.example.com/docs/resource1.html"));
+		fullBrowser.visit(new URL("https://www.google.com/"));
+		fullBrowser.visit(new URL("https://www.youtube.com/"));
+		
+		fullBrowser.visit(new URL("http://www.example.com/docs/resource1.html"));
+		fullBrowser.visit(new URL("https://www.google.com/"));
+		fullBrowser.visit(new URL("https://www.youtube.com/"));
+		
+		fullBrowser.back();
+		fullBrowser.back();
+		fullBrowser.back();
+	}
+	
+	
+	
+//========================================================= CONSTRUCTOR TESTS ========================================================================
+	
+	
+	@Test
+	public static void constructorTestOnEmpty () throws MalformedURLException {
+		setup();
+		
+		
+	}
+	
+	
+	
+	@Test
+	public static void constructorTestOnSmall () throws MalformedURLException {
+		setup();
+		
+	}
+	
+	
+	
+	@Test
+	public static void constructorTestOnLarge () throws MalformedURLException {
+		setup();
+		
+	}
+	
+	
+	
+//=========================================================== VISIT TESTS ============================================================================
+	
+	
+	@Test
+	public static void visitTestOnEmpty () throws MalformedURLException {
+		setup();
+		
+	}
+	
+	
+	
+	@Test
+	public static void visitTestOnSmall () throws MalformedURLException {
+		setup();
+		
+	}
+	
+	
+	
+	@Test
+	public static void visitTestOnLarge () throws MalformedURLException {
+		setup();
+		
+	}
+	
+	
+	
+//=========================================================== BACK TESTS =============================================================================
+	
+	
+	@Test
+	public static void backTestOnEmpty () throws MalformedURLException {
+		setup();
+		
+	}
+	
+	
+	
+	@Test
+	public static void backTestOnSmall () throws MalformedURLException {
+		setup();
+		
+	}
+	
+	
+	
+	@Test
+	public static void backTestOnLarge () throws MalformedURLException {
+		setup();
+		
+	}
+	
+	
+	
+//========================================================== FORWARD TESTS ===========================================================================
+	
+	
+	@Test
+	public static void forwardTestOnEmpty () throws MalformedURLException {
+		setup();
+		
+	}
+	
+	
+	
+	@Test
+	public static void forwardTestOnSmall () throws MalformedURLException {
+		setup();
+		
+	}
+	
+	
+	
+	@Test
+	public static void forwardTestOnLarge () throws MalformedURLException {
+		setup();
+		
+	}
+	
+	
+	
+//========================================================== HISTORY TESTS ===========================================================================
+
+	
+	@Test
+	public static void historyTestOnEmpty () throws MalformedURLException {
+		setup();
+		
+	}
+	
+	
+	
+	@Test
+	public static void historyTestOnSmall () throws MalformedURLException {
+		setup();
+		
+	}
+	
+	
+	
+	@Test
+	public static void historyTestOnLarge () throws MalformedURLException {
+		setup();
+		
+	}
+	
 }
