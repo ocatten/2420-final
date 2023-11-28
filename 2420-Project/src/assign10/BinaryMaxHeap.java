61ecc7121095f71cd705a5094997d5ff2fd941d1
EverettOglesby <everett.s.oglesby@gmail.com>
2023-11-27 22:32:34 -0700

    Update BinaryMaxHeap.java

--- 2420-Project/src/assign10/BinaryMaxHeap.java
+++ 2420-Project/src/assign10/BinaryMaxHeap.java
@@ -5,52 +5,52 @@ import java.util.List;
 import java.util.NoSuchElementException;
 
 /*
- * This method is used to create and interact with a binary max heap. It implements the PriorityQueue interface
- * and is able to uses applicable methods such as add, clear, remove, etc. The binary max heap is represented 
+ * This method is used to create and interact with a binary max heapBackingArr. It implements the PriorityQueue interface
+ * and is able to uses applicable methods such as add, clear, remove, etc. The binary max heapBackingArr is represented 
  * as a tree with each node being larger than the nodes below it. When a node is added to the tree, it is added
  * to the next available space and is percolated up until all the nodes directly above it are larger and all the 
  * nodes below are smaller.	
  * 
  * @Authors Everett Oglesby and Parker Catten
  */
-public class BinaryMaxHeap<E> implements PriorityQueue<E>{
+public class BinaryMaxheapBackingArr<E> implements PriorityQueue<E>{
 	
-	E[] backingArray;//Stores the heap as a basic array.
-	int backingArrLength;//Stores the length of the backing array.
+	E[] heapBackingArr;//Stores the heapBackingArr as a basic array.
+	int heapBackingArrLength;//Stores the length of the backing array.
 	
-	int size; //Tracker for the number of items in the binary heap.
+	int size; //Tracker for the number of items in the binary heapBackingArr.
 
 	private Comparator<? super E> cmp;//Empty comparator to be replaced if a comparator is given.
 	/*
-	 * This constructor is used to create a empty binary heap and
+	 * This constructor is used to create a empty binary heapBackingArr and
 	 * it assumes elements are ordered using their natural ordering.
 	 */
 	@SuppressWarnings("unchecked")
-	public BinaryMaxHeap() {
+	public BinaryMaxheapBackingArr() {
 		
 	}
 
 	/*
-	 * If this constructor is used to create a empty binary heap, then elements
+	 * If this constructor is used to create a empty binary heapBackingArr, then elements
 	 * are ordered using the provided Comparator.
 	 */
-	public BinaryMaxHeap(Comparator<? super E>) {
+	public BinaryMaxheapBackingArr(Comparator<? super E>) {
 		
 	}
 
 	/*
-	 * This constructor creates a binary heap from the given list using the
+	 * This constructor creates a binary heapBackingArr from the given list using the
 	 * given element's natural ordering.
 	 */
-	public BinaryMaxHeap(List<? extends E>) {
+	public BinaryMaxheapBackingArr(List<? extends E>) {
 		
 	}
 	
 	/*
-	 * This constructor creates a binary heap from the list of generic elements
+	 * This constructor creates a binary heapBackingArr from the list of generic elements
 	 * using the given comparator.
 	 */
-	public BinaryMaxHeap(List<? extends E>, Comparator<? super E>) {
+	public BinaryMaxheapBackingArr(List<? extends E>, Comparator<? super E>) {
 		
 	}
 
@@ -117,8 +117,10 @@ public class BinaryMaxHeap<E> implements PriorityQueue<E>{
 	 * O(1)
 	 */
 	public void clear() {
-		// TODO Auto-generated method stub
-		
+		//Clear the heap by setting each value to default
+		heapBackingArrLength = 3;
+		heapBackingArr = (E[]) new Object[heapBackingArrLength];
+		size = 0;
 	}
 
 	/** 
@@ -131,8 +133,23 @@ public class BinaryMaxHeap<E> implements PriorityQueue<E>{
 	 * whether it is in stored there in the backing array.) 
 	 */
 	public Object[] toArray() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Object[] toArr = new Object[size];//Object array to be returned
+		
+		int toArrIndex = 0;//Tracker for the current object to be placed in the toArr
+		
+		
+		//Loop through the whole backing array
+		for(int i = 0; i < heapBackingArr.length;i++) {
+			
+			//If the current object isn't null then add it to the next open position in the toArr.
+			if(heapBackingArr != null) {
+				toArr[toArrIndex] = heapBackingArr[i];
+				toArrIndex++;
+			}
+		}
+		
+		return toArr;
 	}
 	
 	
@@ -167,43 +184,43 @@ public class BinaryMaxHeap<E> implements PriorityQueue<E>{
 	}
 	
 	/*
-	 * Builds a binary heap from the given generic list.
+	 * Builds a binary heapBackingArr from the given generic list.
 	 */
-	public void buildHeap(List<? extends E> list) {
+	public void buildheapBackingArr(List<? extends E> list) {
 		//Creates a backing array with its size being a full last level of the tree representation.
 		int i = 1;
-		while(list.size() >= backingArrLength) {
-			backingArrLength = 2^i;
+		while(list.size() >= heapBackingArrLength) {
+			heapBackingArrLength = 2^i;
 			i++;
 		}
-		backingArray = (E[]) new Object[backingArrLength];
+		heapBackingArr = (E[]) new Object[heapBackingArrLength];
 		
-		// Add each element from the list to the binary heap.
+		// Add each element from the list to the binary heapBackingArr.
 		for(int j = 0; j < list.size(); j++) {				
-			backingArray[list.size()+1 - (i+2)] = list.get(i);
+			heapBackingArr[list.size()+1 - (i+2)] = list.get(i);
 
 			size++;
 		}
 	}
 	
 	/*
-	 * Percolates the current item up the binary max heap until no larger smaller item exists
+	 * Percolates the current item up the binary max heapBackingArr until no larger smaller item exists
 	 * directly above it. The given index is the position of the item being perculated up.
 	 */
 	public void perculateUp(int index) {
 		
 		
 		//Determines if the current node is greater than its parent
-		if (innerCompare(backingArray[index], backingArray[(index-1)/2]) > 0) {
+		if (innerCompare(heapBackingArr[index], heapBackingArr[(index-1)/2]) > 0) {
 			
 			//Data at the current index
-			E lowerData = backingArray[index];
+			E lowerData = heapBackingArr[index];
 			
 			//Since the child is larger, swap the parent and child.
-			backingArray[index] = backingArray[ (index-1)/2 ];
-			backingArray[ (index-1)/2 ] = lowerData;
+			heapBackingArr[index] = heapBackingArr[ (index-1)/2 ];
+			heapBackingArr[ (index-1)/2 ] = lowerData;
 			
-			//Check the parent to see if the element can be traverse the heap even higher.
+			//Check the parent to see if the element can be traverse the heapBackingArr even higher.
 			perculateUp((index-1)/2);
 			
 		} else {
@@ -213,7 +230,7 @@ public class BinaryMaxHeap<E> implements PriorityQueue<E>{
 	
 	
 	/*
-	 * Percolates the current item down the binary heap until no larger item exists anywhere below it. The 
+	 * Percolates the current item down the binary heapBackingArr until no larger item exists anywhere below it. The 
 	 * 
 	 */
 	public void perculateDown() {
